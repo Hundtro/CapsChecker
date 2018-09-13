48955e8675b46db83b4a0234bea636c2d585f820
Dmytro Svynarchuk <dsvynarchuk@sii.pl>
2018-09-13 17:46:46 +0200

    fix tabulation

--- Program.cs
+++ Program.cs
@@ -3,14 +3,14 @@ using System.Windows.Forms;
 
 namespace CapsChecker
 {
-	class Program
-	{
-		[STAThread]
-		public static void Main(string[] args)
-		{
-			Application.EnableVisualStyles();
+    class Program
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
             Application.Run(new Checker());
-		}
-	}
+        }
+    }
 }
--- Settings.cs
+++ Settings.cs
@@ -1,13 +1,13 @@
 ﻿namespace CapsChecker
 {
-	public static class Settings
-	{
-		public const int nWaitTime = 1000;
-		public const string nName = "CapsChecker";
-		public const string nMenuAbout = "About";
-		public const string nMenuExit = "Exit";
-		public const string nTitle = "Checker message";
-		public const string nMessageOn = "Is ON";
-		public const string nMessageOff = "Is OFF";
-	}
+    public static class Settings
+    {
+        public const int nWaitTime = 1000;
+        public const string nName = "CapsChecker";
+        public const string nMenuAbout = "About";
+        public const string nMenuExit = "Exit";
+        public const string nTitle = "Checker message";
+        public const string nMessageOn = "Is ON";
+        public const string nMessageOff = "Is OFF";
+    }
 }
